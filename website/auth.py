b723298e76d0c82857ed7104d76e90736c89b1cc
IT24102362 <it24102362@my.sliit.lk>
2026-02-04 23:14:31 +0530

    Sign-up done

--- website/auth.py
+++ website/auth.py
@@ -1,15 +1,32 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, flash
 
 auth = Blueprint('auth', __name__)
 
-@auth.route('/login')
+@auth.route('/login', methods=['GET', 'POST'])
 def login():
-    return render_template("login.html")
+    return render_template("login.html", boolean = True)
 
 @auth.route('/logout')
 def logout():
     return "<p>Logout</p>"
 
-@auth.route('/sign-up')
+@auth.route('/sign-up', methods=['GET', 'POST'])
 def sign_up():
+    if request.method == 'POST':
+        email = request.form.get('email')
+        firstname = request.form.get('firstname')
+        password1 = request.form.get('password1')
+        password2 = request.form.get('password2')
+
+        if len(email) < 4:
+            flash('Email must be at least 4 characters long.', category='error')
+        elif len(firstname) < 2:
+            flash('First name must be at least 2 characters long.', category='error')
+        elif password1 != password2:
+            flash('Passwords do not match.', category='error')
+        elif len(password1) < 8:
+            flash('Password must be at least 8 characters long.', category='error')
+        else:
+            flash('Account created successfully.', category='success')
+            #add user to database
     return render_template("sign_up.html")
